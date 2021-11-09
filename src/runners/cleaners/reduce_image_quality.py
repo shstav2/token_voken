e56aaeea10aa636af4c04fe24a51ac92998acefb
stavshem <stav.shem@gmail.com>
2021-11-09 09:26:54 +0200

    Finish reducing oliver annotated faces

--- src/runners/cleaners/reduce_image_quality.py
+++ src/runners/cleaners/reduce_image_quality.py
@@ -16,14 +16,19 @@ interval_ids = ['104792']
 oliver_interval_ids = list(INTERVAL_TO_VIDEO_OLIVER.keys())
 
 interval_ids = ['105339']
-for interval_id in oliver_interval_ids[100:1000]:
+
+
+for interval_id in interval_ids:
     # /home/stav/Data/PATS_DATA/Videos/oliver/Nn_Zln_4pA8/104792/FacesAll
     dir_faces_all = resolve_interval_all_faces_dir(interval_id)
     # ['/home/stav/Data/PATS_DATA/Videos/oliver/Nn_Zln_4pA8/104792/FacesAll/00213',
     #  '/home/stav/Data/PATS_DATA/Videos/oliver/Nn_Zln_4pA8/104792/FacesAll/00214']
     frame_faces_dirs = sorted(listdir_nohidden(dir_faces_all))
     for all_faces_of_single_frame_dir in frame_faces_dirs:
-        command = REDUCE_IMAGE_QUALITY_COMMAND.format(faces_all=all_faces_of_single_frame_dir, quality=REDUCTED_QUALITY)
-        run_command(command)
-        original_annotated_faces = os.path.join(all_faces_of_single_frame_dir, 'annotated_faces.jpg')
-        delete_file(original_annotated_faces)
+        try:
+            command = REDUCE_IMAGE_QUALITY_COMMAND.format(faces_all=all_faces_of_single_frame_dir, quality=REDUCTED_QUALITY)
+            run_command(command)
+            original_annotated_faces = os.path.join(all_faces_of_single_frame_dir, 'annotated_faces.jpg')
+            delete_file(original_annotated_faces)
+        except Exception as e:
+            print(f'ERROR: {interval_id}, frame {all_faces_of_single_frame_dir[-10:]}, {e}')
