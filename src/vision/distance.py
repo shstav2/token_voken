d21ab99650e3c9d54d6a13fb9d3614b543e38e77
Stav <stavshem@users.noreply.github.com>
2022-12-22 15:00:14 +0200

    Distance between images using norm

--- src/vision/distance.py
+++ src/vision/distance.py
@@ -6,19 +6,20 @@ import numpy as np
 
 # LFW functions taken from David Sandberg's FaceNet implementation
 def distance(embeddings1, embeddings2, distance_metric='euc'):
-    if distance_metric == 'euc':
-        # Euclidian distance
-        diff = np.subtract(embeddings1, embeddings2)
-        dist = np.sum(np.square(diff))
-    elif distance_metric == 'cos':
-        # Distance based on cosine similarity
-        dot = np.sum(np.multiply(embeddings1, embeddings2))
-        norm = np.linalg.norm(embeddings1) * np.linalg.norm(embeddings2)
-        similarity = dot / norm
-        dist = np.arccos(similarity) / math.pi
-    else:
-        raise 'Undefined distance metric %d' % distance_metric
-    return dist
+    return np.linalg.norm(embeddings1-embeddings2)
+#     if distance_metric == 'euc':
+#         # Euclidian distance
+#         diff = np.subtract(embeddings1, embeddings2)
+#         dist = np.sum(np.square(diff))
+#     elif distance_metric == 'cos':
+#         # Distance based on cosine similarity
+#         dot = np.sum(np.multiply(embeddings1, embeddings2))
+#         norm = np.linalg.norm(embeddings1) * np.linalg.norm(embeddings2)
+#         similarity = dot / norm
+#         dist = np.arccos(similarity) / math.pi
+#     else:
+#         raise 'Undefined distance metric %d' % distance_metric
+#     return dist
 
 
 # TODO: Generalize
